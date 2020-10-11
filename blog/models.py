a6cb0490b14055e054e554168ea9cd525a61adb0
Matt <ihate2020scam@gmail.com>
2020-10-11 15:07:01 +0100

    Create Home page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    publication_date = models.DateTimeField()
+    blog_content = models.TextField()
+    image2 = models.ImageField(upload_to = 'images/')
