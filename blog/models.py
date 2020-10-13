ba83af80a8110aaef1c3fff6ac546cd393c5ac90
Matt <ihate2020scam@gmail.com>
2020-10-13 15:48:08 +0100

    Blog details and STATIC FILES

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,13 @@ class Blog(models.Model):
     publication_date = models.DateTimeField()
     blog_content = models.TextField()
     image2 = models.ImageField(upload_to = 'images/')
+
+    def __str__(self):
+        return self.title #zamienia na stornie admin domyslne listy (blog object 1, blog object 2) na liste z title blogow
+
+    def summary(self):
+        return self.blog_content[:100]
+
+    def publication_date_notime(self):
+        return self.publication_date.strftime('%e %b %Y')
+        # skraca czas tylko do daty, posortowane przy uzyciu srtftime (https://strftime.org/)
